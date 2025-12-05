8f9364ee0815cef2f0d727d93616ff10977ae0a2
samerrizk76 <samerrizk76@gmail.com>
2025-12-05 14:48:49 +0200

    i

---
+++ Select.cpp
@@ -0,0 +1,53 @@
+#include "Select.h"
+
+#include "ApplicationManager.h"
+
+#include "GUI\input.h"
+#include "GUI\Output.h"
+
+#include <sstream>
+using namespace std;
+
+//constructor: set the ApplicationManager pointer inside this action
+Select::Select(ApplicationManager* pAppManager) : Action(pAppManager)
+{
+}
+
+void Select::ReadActionParameters()
+{
+    Input* pIn = pManager->GetInput();
+    Output* pOut = pManager->GetOutput();
+
+    //Read the (Position) parameter
+    pOut->PrintMessage("Select Statement: Click to add the statement");
+
+    pIn->GetPointClicked(Position);
+    pOut->ClearStatusBar();
+
+}
+
+void Select::Execute()
+{
+    ReadActionParameters();
+
+    Statement* clickedStat = pManager->GetStatement(Position);
+
+    if (clickedStat)
+    {
+        Statement* prevSelected = pManager->GetSelectedStatement();
+        if (prevSelected == clickedStat)
+        {
+            clickedStat->SetSelected(false);
+            pManager->SetSelectedStatement(nullptr);
+        }
+        else
+        {
+            if (prevSelected)
+                prevSelected->SetSelected(false);
+
+            clickedStat->SetSelected(true);
+            pManager->SetSelectedStatement(clickedStat);
+        }
+        pManager->UpdateInterface();
+    }
+}
---
+++ Select.h
@@ -0,0 +1,28 @@
+#ifndef SELECT_H
+#define SELECT_H
+
+#include "Actions\Action.h"
+
+class Select : public Action
+{
+private:
+
+
+	bool selected;
+	Point Position;
+
+	//Position where the user clicks to add the stat.
+	//TODO: you should add LHS and RHS of the assignment statement as parameters
+
+public:
+	Select(ApplicationManager* pAppManager);
+
+	//Read Assignemt statements position
+	void ReadActionParameters();
+
+	//Create and add an assignemnt statement to the list of statements
+	void Execute();
+
+};
+
+#endif
